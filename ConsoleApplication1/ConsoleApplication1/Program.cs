6a1c973a3aed0ca24dc90b0d16e9a2bea500832b
holly ireland <holly.ireland@vizolution.co.uk>
2016-10-05 11:30:58 +0100

    updating the test solution to go through the process of committing from visual studio

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("yo");
+            Console.WriteLine("yo dude");
         }
     }
 }
